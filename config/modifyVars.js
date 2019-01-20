657e20b15f0402694fe29c1311fac6f347ab9de8
zlh <zlinhui6@qq.com>
2019-01-20 12:57:17 +0800

    添加预定义css参数

---
+++ config/modifyVars.js
@@ -0,0 +1,19 @@
+module.exports = {
+	//颜色
+	'@titleColor': '#333333', //标题等需要醒目的地方
+	'@defaultColor': '#595959', //正文等不需要过于正式的地方
+	'@promptColor': '#8c8c8c', //文本框中的提示文字或是注释等
+	'@borderColor': '#ddd', //边框
+	'@warningColor': '#faad14', //告警
+	'@dangerColor': '#f5222d', //错误等需要警示的地方
+	'@primaryColor': '#1890ff', //链接，提示等
+	'@backgroundColor': '#f5f5f5', //背景
+	'@moneyColor': '#1890ff', //金额提示等
+	'@runingColor': '#52c41a', //运行状态等
+	//字体大小
+	'@bigSize': '24px', //大图标
+	'@bolderSize': '18px', //一级列表
+	'@boldSize': '16px', //二级标题  块级标题/Table
+	'@normalSize': '14px', //正文内容
+	'@smallSize': '12px' //提示/说明信息
+};
